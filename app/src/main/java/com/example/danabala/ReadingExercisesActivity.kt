251c2af5952010a759cef8c87d6eae7a4d89801a
Timur Sarsembai <sarsembai.timur@gmail.com>
2025-08-05 22:08:23 +0500

    Initial commit

---
+++ app/src/main/java/com/example/danabala/ReadingExercisesActivity.kt
@@ -0,0 +1,49 @@
+package com.example.danabala
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ReadingExercisesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reading_exercises)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        setupBackButton()
+        setupExerciseCards()
+    }
+
+    private fun setupBackButton() {
+        findViewById<ImageView>(R.id.btnBack).setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun setupExerciseCards() {
+        findViewById<CardView>(R.id.cardLetters).setOnClickListener {
+            // TODO: Изучение букв
+        }
+
+        findViewById<CardView>(R.id.cardSyllables).setOnClickListener {
+            // TODO: Слоги
+        }
+
+        findViewById<CardView>(R.id.cardWords).setOnClickListener {
+            // TODO: Слова
+        }
+
+        findViewById<CardView>(R.id.cardSentences).setOnClickListener {
+            // TODO: Предложения
+        }
+    }
+}
